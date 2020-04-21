e4d91c3b5b682354f4957e2eff948a2972fed782
zjulkw <63395600+zjulkw@users.noreply.github.com>
2020-04-21 21:40:18 +0800

    如果端口数为1则自动建立连接，如果端口数大于1则需要手动收入端口号，解决多端口号只连接第一个的问题

--- main.py
+++ main.py
@@ -6,18 +6,23 @@ import serial.tools.list_ports
 data1 = '7a7d7e'
 data2 = '7f7c7b'
 
-#查看可用端口并自动建立连接
-#s = serial.Serial("COM9", 115200)  # 初始化串口
-plist = list(serial.tools.list_ports.comports())
-print(plist)
-if len(plist) <= 0:
+#查看可用端口并建立连接,如果端口数为1则自动建立连接，如果端口数大于1则需要手动输入端口号
+
+plist = list(serial.tools.list_ports.comports()) #获取当前设备的所有COM端口
+port_number = len(plist)
+
+if port_number <= 0:
     print("没有发现端口!")
+elif port_number == 1:
+    print('可用端口数为:1')
+    print('可用端口号>>>' + plist[0][0])
+    s = serial.Serial(plist[0][0], 115200, timeout=6)
 else:
-    plist_0 = list(plist[0])
-    serialName = plist_0[0]
-    serialFd = serial.Serial(serialName, 115200, timeout=6) #波特率设为115200
-    s = serialFd
-    print("可用端口名>>>", serialFd.name)
+    print('可用端口数为'+str(port_number))
+    for i in range(0, len(plist)):
+        print('可用端口号>>>'+plist[i][0])        #打印所有可用的端口号
+    Get_Port_Number = input('请输入连接的端口号：')
+    s = serial.Serial(Get_Port_Number, 115200, timeout=6)
 
 
 print('进入配置模式')
